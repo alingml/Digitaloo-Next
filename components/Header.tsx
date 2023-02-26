a6e6fa6b1a700ce41a2610809150384341452813
Alin Gamalan <alin.gamalan@gmail.com>
2023-02-26 15:07:40 +0200

    added styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,51 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from "framer-motion"
+
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
-        <div className='flex flex-row items-center'>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <motion.div 
+        initial={{
+          x:-500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x:0,
+          opacity:1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5,
+        
+        }}
+        className='flex flex-row items-center'>
             <SocialIcon
                 url="https://www.youtube.com"
                 fgColor='gray'
                 bgColor='transparent'
                 />
-        </div>
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        </motion.div>
+        <motion.div 
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      
+        className='flex flex-row items-center text-gray-300 cursor-pointer'>
             <SocialIcon
                 className='cursor-pointer'
                 network='email'
@@ -22,7 +54,7 @@ export default function Header({}: Props) {
                 />
 
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
-        </div>
+        </motion.div>
     </header>
   )
 }
